5cf96a4c10e84035078db019553aeed676731945
Jingxuan <liujingxuanowen@gmail.com>
2024-11-19 11:41:43 -0500

    first init: add frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [contacts, setContacts] = useState([])
+
+  const fetchContacts = async () => {
+    const response = await fetch ("http://localhost:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+
+  useEffect(() => {
+    fetchContacts();
+  }, [])
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
